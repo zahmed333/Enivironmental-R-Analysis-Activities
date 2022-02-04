f704ad04df4073cb534af8338a6ae90de5b39780
Zul <82294272+zahmed333@users.noreply.github.com>
2022-02-04 12:09:57 -0500

    Update Activity2.R
    reading data

--- Activity2.R
+++ Activity2.R
@@ -1,7 +1,13 @@
 #GEOG331 Activity 2
 # Z.A. 01/31/2022
 
+# set working directory to my noaa data folder
+
 # make a vector for practice
 heights <- c(20, 30, 40, 50)
 
 heights^2
+
+
+#reads the data from file
+datW <- read.csv("Z:/data/noaa_weather/2011124.csv")
