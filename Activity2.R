3da894d6134aa5c18b89ed35f3c5c1b4eb648a9d
Zul <82294272+zahmed333@users.noreply.github.com>
2022-02-07 12:03:34 -0500

    Update Activity2.R
    question 2

--- Activity2.R
+++ Activity2.R
@@ -11,3 +11,18 @@ heights^2
 
 #reads the data from file
 datW <- read.csv("Z:/data/noaa_weather/2011124.csv")
+
+#Question 2 vectors
+
+#Numeric
+die <- c(1.1, 2.2, 3.3, 4.4, 5.5, 6.6)
+
+#Integer
+int <- c(-1L, 2L, 4L, 3L, -5L)
+
+#Character
+text <- c("Hello", "Professor", "Loranty,", "I am", "Zul")
+
+#Factor Data
+eyes <- c("Brown", "Black", "Hazel", "Green", "Blue")
+
