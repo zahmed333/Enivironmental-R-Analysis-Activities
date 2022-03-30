f5753ad7c4ac92afe7b45bff17e67c1720e7d8dc
zahmed333 <zahmed@colgate.edu>
2022-03-30 12:22:02 -0400

    9

--- Activity5.R
+++ Activity5.R
@@ -51,7 +51,7 @@ plot(datD$decYear, datD$discharge, type="l", xlab="Year", ylab=expression(paste(
 
 
 #QUESTION 5
-+
+
 #basic formatting
 aveF <- aggregate(datD$discharge, by=list(datD$doy), FUN="mean")
 colnames(aveF) <- c("doy","dailyAve")
@@ -82,11 +82,77 @@ axis(1, seq(0,360, by=30), #tick intervals
 axis(2, seq(0,80, by=20),
      seq(0,80, by=20),
      las = 2)#show ticks at 90 degree angle
-legend("topright", c("mean","1 standard deviation"), #legend items
-       lwd=c(2,NA),#lines
-       col=c("black",rgb(0.392, 0.584, 0.929,.2)),#colors
+#2017 line
+lines(datD$discharge[datD$year == "2017"],
+     col = "blue", pch = 15)
+
+legend("topright", c("mean","1 standard deviation", "2017 stream flow"), #legend items
+       lwd=c(3,NA),#lines
+       col=c("black",rgb(0.392, 0.584, 0.929,.2), "blue"),#colors
        pch=c(NA,15),#symbols
        bty="n")#no legend border
-#2017 line
-line(datD$discharge[datD$year == "2017"],
-     color = "blue", pch = 15)
+
+
+
+
+
+
+
+
+#subsest discharge and precipitation within range of interest
+hydroD <- datD[datD$doy >= 248 & datD$doy < 250 & datD$year == 2011,]
+hydroP <- datP[datP$doy >= 248 & datP$doy < 250 & datP$year == 2011,]
+
+
+min(hydroD$discharge)
+
+#get minimum and maximum range of discharge to plot
+#go outside of the range so that it's easy to see high/low values
+#floor rounds down the integer
+yl <- floor(min(hydroD$discharge))-1
+#ceiling rounds up to the integer
+yh <- ceiling(max(hydroD$discharge))+1
+#minimum and maximum range of precipitation to plot
+pl <- 0
+pm <-  ceiling(max(hydroP$HPCP))+.5
+#scale precipitation to fit on the 
+hydroP$pscale <- (((yh-yl)/(pm-pl)) * hydroP$HPCP) + yl
+
+par(mai=c(1,1,1,1))
+#make plot of discharge
+plot(hydroD$decDay,
+     hydroD$discharge, 
+     type="l", 
+     ylim=c(yl,yh), 
+     lwd=2,
+     xlab="Day of year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+#add bars to indicate precipitation 
+for(i in 1:nrow(hydroP)){
+  polygon(c(hydroP$decDay[i]-0.017,hydroP$decDay[i]-0.017,
+            hydroP$decDay[i]+0.017,hydroP$decDay[i]+0.017),
+          c(yl,hydroP$pscale[i],hydroP$pscale[i],yl),
+          col=rgb(0.392, 0.584, 0.929,.2), border=NA)
+}
+
+## Question 8
+
+
+
+
+library(ggplot2)
+#specify year as a factor
+datD$yearPlot <- as.factor(datD$year)
+#make a boxplot
+ggplot(data= datD, aes(yearPlot,discharge)) + 
+  geom_boxplot()
+
+
+#make a violin plot
+ggplot(data= datD, aes(yearPlot,discharge)) + 
+  geom_violin()
+
+# Question 9
+datD$dose <- as.factor(ToothGrowth$dose)
+ggplot(data= datD, aes(yearPlot == "2016",discharge)) + 
+  geom_violin()
